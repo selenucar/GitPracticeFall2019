1bc1f151f99923ade701c000d7c19b8dc3ff526f
selenucar <slnucartx@gmail.com>
2020-03-11 18:46:53 -0500

    first commit

---
+++ src/day1/StringPractice.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        String str = "Java is fun!";
+        System.out.println(str);
+    }
+}
